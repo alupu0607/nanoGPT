c574ff24cf487e04a9ded91f686b0b174adf4564
Andreea Lupu <0607andreea.ro@gmail.com>
2026-06-11 20:08:56 +0300

    train sweeps for h2

--- h2/h2_train_sweep.py
+++ h2/h2_train_sweep.py
@@ -4,7 +4,7 @@ import subprocess, os, time, torch, json, math
 # M_VALUES = [1, 5, 10, 20]
 
 RUN_ORDER  = [760, 512, 256]
-M_VALUES = [1,5,10, 20]
+M_VALUES = [10, 20]
 TASK      = "wikitext2"
 
 results         = []
